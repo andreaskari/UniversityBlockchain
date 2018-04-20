3677a0848dfcd717de44bde67de43c2d0e4ef342
Andre <andre@Gonul-MacBook.local>
2018-04-19 18:39:22 -0700

    Debuggin before adding networking

--- universitynode.py
+++ universitynode.py
@@ -10,35 +10,43 @@ from uuid import uuid4
 from flask import Flask
 
 
+# -- Constants -- #
+
+INSTITUTION_INFO_FILE_PATH = './institution.json'
+
+
 # -- Classes -- #
 
 class UniversityNode(object):
-    def __init__(self, institution_name):
+    def __init__(self, institution_properties):
         # Properties all blockchain nodes have
         self.chain = []
         self.current_student_records = []
 
-        # Create the genesis block
-        self.new_block(previous_hash=1, proof=100)
-
         # Private properties (for this node)
-        self.institution_name = institution_name
-        self.institution_signature = SIGN(self.institution_name)
-        
+        self.institution_name = institution_properties['name']
+        self.institution_signature = self.institution_name
+        # self.institution_signature = SIGN(self.institution_name)
+        self.institution_public_key = institution_properties['public_key']
+        self.institution_private_key = institution_properties['private_key']
 
-    def get_current_block(self, proof):
+        # Create the genesis block
+        self.append_new_block(self.get_current_block(proof=100, previous_hash=1))
+
+
+    def get_current_block(self, proof, previous_hash=None):
         # Creates a new Block and adds it to the chain
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
             'student_records': self.current_student_records,
             'proof': proof,
-            'previous_hash': self.hash(self.chain[-1]),
+            'previous_hash': previous_hash or hash(self.chain[-1]),
         }
         return block
 
     
-    def new_record(self, first_name, last_name, id_number, date_enrolled_through):
+    def add_new_record(self, first_name, last_name, id_number, date_enrolled_through):
         # Adds a new record to the list of transactions
         new_record_contents = {
             'institution_name': self.institution_name,
@@ -65,15 +73,22 @@ class UniversityNode(object):
         self.chain.append(block)
 
 
-    def proof_of_work(self, last_proof):
+    def proof_of_work(self):
         # Find a proof that works for this block 
         # This is a hard task
+        # Returns a valid block
         proof = 0
         current_block = self.get_current_block(proof)
         while valid_block_proof(current_block) is False:
             proof += 1
             current_block = self.get_current_block(proof)
-        return proof
+        return current_block
+
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        return self.chain[-1]
 
 
     @staticmethod
@@ -103,12 +118,6 @@ class UniversityNode(object):
         return hashlib.sha256(block_string).hexdigest()
 
 
-    @property
-    def last_block(self):
-        # Returns the last Block in the chain
-        return self.chain[-1]
-
-
 # -- Endpoints -- #
 
 
@@ -118,25 +127,48 @@ app = Flask(__name__)
 # Generate a globally unique address for this node
 node_identifier = str(uuid4()).replace('-', '')
 
-# Instantiate the Blockchain
-node = UniversityNode()
+# Instantiate the Node
+with open(INSTITUTION_INFO_FILE_PATH) as data_file:    
+    institution_properties = json.load(data_file)
+node = UniversityNode(institution_properties)
 
 
 @app.route('/mine', methods=['GET'])
 def mine():
-    return "We'll mine a new Block"
+    # We run the proof of work algorithm to get a valid block
+    valid_block = node.proof_of_work()
+
+    response = {
+        'message': "New Block Forged",
+        'block': valid_block,
+    }
+    return jsonify(response), 200
+
   
-@app.route('/transactions/new', methods=['POST'])
-def new_transaction():
-    return "We'll add a new transaction"
+@app.route('/record/new', methods=['POST'])
+def new_record():
+    values = request.get_json()
+
+    # Check that the required fields are in the POST'ed data
+    required = ['first_name', 'last_name', 'id_number', 'date_enrolled_through']
+    if not all(k in values for k in required):
+        return 'Missing values', 400
+
+    # Create a new Transaction
+    index = node.add_new_record(self, values['first_name'], values['last_name'], values['id_number'], values['date_enrolled_through'])
+
+    response = {'message': f'Transaction will be added to Block {index}'}
+    return jsonify(response), 201
+
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
     response = {
-        'chain': blockchain.chain,
-        'length': len(blockchain.chain),
+        'chain': node.chain,
+        'length': len(node.chain),
     }
     return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
